d8b89f863dfcb6d6f3f7f6563d21a216e784a11f
racyuan <racyuan@cisco.com>
2020-01-14 20:19:31 -0500

    added custom tagging

--- tag.py
+++ tag.py
@@ -1,6 +1,9 @@
 import nltk
 from nltk.tag.stanford import StanfordNERTagger
 
+#java -cp "stanford-ner.jar:lib/*" -mx12g edu.stanford.nlp.ie.crf.CRFClassifier -prop ../my-little-ner/prop.txt
+
+#STANFORD TAGGING
 jar = './stanford-ner/stanford-ner.jar'
 stanford = './stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz'
 
@@ -11,3 +14,9 @@ words = nltk.word_tokenize(sentence)
 tags = stanford_ner.tag(words)
 
 print(tags)
+
+#CUSTOM TAGGING
+#ner = './my-little-ner/classifiers/ner-model.ser.gz'
+#my_ner = StanfordNERTagger(ner, jar, encoding='utf8')
+#my_tags = my_ner.tag(words)
+#print(my_tags)
